4127a4f1a72deacb427f2b52805946f2bc05fe8a
SlimCandy <cgpnrjmhppeb@ro.ru>
2023-02-25 21:42:27 +0100

    Update Types

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -2,89 +2,86 @@ import { CreatePagesArgs, GatsbyNode } from "gatsby"
 import path from "path"
 import { TCategoryContext, TPostContext } from "./src/utils/types"
 
-const turnPostsIntoPages: GatsbyNode["createPages"] =
-  async function turnPostsIntoPages({ graphql, actions }) {
-    // 1. Get a template for this page
-    const postTemplate = path.resolve("./src/templates/Post.tsx")
-    // 2. Query all posts
-    const {
-      data,
-    }: {
-      data?: {
-        allSanityPost: {
-          nodes: TPostContext[]
-        }
+async function turnPostsIntoPages({ graphql, actions }: CreatePagesArgs) {
+  // 1. Get a template for this page
+  const postTemplate = path.resolve("./src/templates/Post.tsx")
+  // 2. Query all posts
+  const {
+    data,
+  }: {
+    data?: {
+      allSanityPost: {
+        nodes: TPostContext[]
       }
-    } = await graphql(`
-      query AllPosts {
-        allSanityPost(sort: { publishedAt: DESC }, limit: 100) {
-          nodes {
-            title
-            slug {
-              current
-            }
+    }
+  } = await graphql(`
+    query AllPosts {
+      allSanityPost(sort: { publishedAt: DESC }, limit: 100) {
+        nodes {
+          title
+          slug {
+            current
           }
         }
       }
-    `)
+    }
+  `)
 
-    const posts = data?.allSanityPost?.nodes
-    // 3. Loop over each post and create a page for that post
-    posts &&
-      posts.forEach(post => {
-        console.log("Creating page for ", post.title)
-        actions.createPage({
-          path: `posts/${post.slug.current}`,
-          component: postTemplate,
-          context: {
-            slug: post.slug.current,
-          },
-        })
-      })
-  }
-
-const turnCategoriesIntoPages: GatsbyNode["createPages"] =
-  async function turnCategoriesIntoPages({ graphql, actions }) {
-    // 1. Get a template for this page
-    const categoryTemplate = path.resolve("./src/templates/PostList.tsx")
-    // 2. Query all categories
-    const {
-      data,
-    }: {
-      data?: {
-        allSanityCategory: {
-          nodes: TCategoryContext[]
-        }
-      }
-    } = await graphql(`
-      query AllCategories {
-        allSanityCategory(sort: { _createdAt: DESC }, limit: 100) {
-          nodes {
-            name
-            slug {
-              current
-            }
-          }
-        }
-      }
-    `)
-
-    const categories = data?.allSanityCategory?.nodes
-    // 3. Loop over each category and create a page for that category
-    categories &&
-      categories.forEach(category => {
-        console.log("Creating page for ", category.name)
-        actions.createPage({
-          path: `categories/${category.slug.current}`,
-          component: categoryTemplate,
-          context: {
-            slug: category.slug.current,
-            name: category.name,
-          },
-        })
-      })
-  }
-
-export async function createPages(params: CreatePagesArgs) {
-  Promise.all([turnPostsIntoPages(params), turnCategoriesIntoPages(params)])
+  const posts = data?.allSanityPost?.nodes || []
+  // 3. Loop over each post and create a page for that post
+  posts.forEach(post => {
+    console.log("Creating page for Article:", post.title)
+    actions.createPage({
+      path: `posts/${post.slug.current}`,
+      component: postTemplate,
+      context: {
+        slug: post.slug.current,
+      },
+    })
+  })
 }
+
+async function turnCategoriesIntoPages({ graphql, actions }: CreatePagesArgs) {
+  // 1. Get a template for this page
+  const categoryTemplate = path.resolve("./src/templates/PostList.tsx")
+  // 2. Query all categories
+  const {
+    data,
+  }: {
+    data?: {
+      allSanityCategory: {
+        nodes: TCategoryContext[]
+      }
+    }
+  } = await graphql(`
+    query AllCategories {
+      allSanityCategory(sort: { _createdAt: DESC }, limit: 100) {
+        nodes {
+          name
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `)
+
+  const categories = data?.allSanityCategory?.nodes || []
+  // 3. Loop over each category and create a page for that category
+  categories.forEach(category => {
+    console.log("Creating page for Category: ", category.name)
+    actions.createPage({
+      path: `categories/${category.slug.current}`,
+      component: categoryTemplate,
+      context: {
+        slug: category.slug.current,
+        name: category.name,
+      },
+    })
+  })
+}
+
+export const createPages: GatsbyNode["createPages"] =
+  async function createPages(params) {
+    Promise.all([turnPostsIntoPages(params), turnCategoriesIntoPages(params)])
+  }
