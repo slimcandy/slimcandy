81ea600813c5fc09478a5e3998bc592a1f195a04
SlimCandy <91788772+slimcandy@users.noreply.github.com>
2023-06-07 21:58:36 +0200

    Add structured data

--- src/templates/Post.tsx
+++ src/templates/Post.tsx
@@ -20,15 +20,19 @@ function SinglePostLayout({ data }: { data: Queries.SinglePostLayoutQuery }) {
     <TinyLayout siteMetadata={siteMetadata}>
       <main className="font-serif my-1 sm:my-2 md:my-3 lg:my-4 sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-3xl mx-auto">
         <article
+          itemScope
+          itemType="http://schema.org/Article"
           className="prose prose-neutral prose-sm sm:prose-base md:prose-lg lg:prose-xl xl:prose-2xl
           prose-a:underline prose-a:decoration-black prose-a:decoration-2 prose-a:underline-offset-2 hover:prose-a:decoration-red-700 focus:prose-a:outline-none focus:prose-a:no-underline focus:prose-a:ring-4 focus:prose-a:ring-offset-2 focus:prose-a:ring-stone-900 focus:prose-a:bg-white"
         >
-          {post?.title && <h1>{post.title}</h1>}
+          {post?.title && <h1 itemProp="headline">{post.title}</h1>}
           {post._rawContent && (
-            <PortableText
-              value={post._rawContent}
-              components={portableTextComponents}
-            />
+            <section itemProp="articleBody">
+              <PortableText
+                value={post._rawContent}
+                components={portableTextComponents}
+              />
+            </section>
           )}
         </article>
       </main>
