5d173f8b2911f589ada3f8b5dfb0d446c2d92d5e
SlimCandy <91788772+slimcandy@users.noreply.github.com>
2023-06-09 20:39:30 +0200

    Add response

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,13 +1,20 @@
 import React from "react"
 import addToMailchimp from "gatsby-plugin-mailchimp"
 
+type TResult = { msg: string; result: string }
+
 function SignUpForm(): JSX.Element {
   const [email, setEmail] = React.useState("")
+  const [result, setResult] = React.useState<TResult>(null)
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
     event.preventDefault()
 
-    addToMailchimp(email).then(console.log).catch(console.error)
+    addToMailchimp(email)
+      .then(function getResult(data: TResult) {
+        setResult(data)
+      })
+      .catch(console.error)
   }
 
   function handleEmailChange(event: React.ChangeEvent<HTMLInputElement>): void {
@@ -17,6 +24,17 @@ function SignUpForm(): JSX.Element {
   return (
     <>
       <hr className="border-t-[thin] border-slate-400 my-4 sm:my-5.5 md:my-6 lg:my-7 xl:my-8" />
+
+      {result !== null && (
+        <div className="flex flex-col items-center justify-center space-y-2">
+          <h3 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold text-slate-400">
+            {result.msg}
+          </h3>
+          <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-light text-slate-400">
+            {result.result}
+          </p>
+        </div>
+      )}
 
       <div className="flex items-end justify-center space-x-4 sm:space-x-5 md:space-x-6 lg:space-x-8 xl:space-x-10">
         <h3 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold text-slate-400">
